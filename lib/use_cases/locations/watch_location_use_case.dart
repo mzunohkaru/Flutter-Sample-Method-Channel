0e3cad3f2708f2354cebe1c74e4389c2d98b5172
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-01-18 23:02:08 +0900

    update:provider

--- lib/use_cases/locations/watch_location_use_case.dart
+++ lib/use_cases/locations/watch_location_use_case.dart
@@ -3,9 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/locations/location_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'get_location_use_case.g.dart';
+part 'watch_location_use_case.g.dart';
 
 @riverpod
-Future<Location> getLocationUseCase(Ref ref) async {
-  return await ref.read(locationRepositoryProvider).get();
+Stream<Location> watchLocationUseCase(Ref ref) {
+  return ref.read(locationRepositoryProvider).watch();
 }
--- lib/use_cases/locations/watch_location_use_case.g.dart
+++ lib/use_cases/locations/watch_location_use_case.g.dart
@@ -1,28 +1,29 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'get_location_use_case.dart';
+part of 'watch_location_use_case.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$getLocationUseCaseHash() =>
-    r'258740b8244f8c7df461594c30c9581fd2f96be7';
+String _$watchLocationUseCaseHash() =>
+    r'e4711c66c1de781b3808c3eb14b6ea5c5e9569d2';
 
-/// See also [getLocationUseCase].
-@ProviderFor(getLocationUseCase)
-final getLocationUseCaseProvider = AutoDisposeFutureProvider<Location>.internal(
-  getLocationUseCase,
-  name: r'getLocationUseCaseProvider',
+/// See also [watchLocationUseCase].
+@ProviderFor(watchLocationUseCase)
+final watchLocationUseCaseProvider =
+    AutoDisposeStreamProvider<Location>.internal(
+  watchLocationUseCase,
+  name: r'watchLocationUseCaseProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$getLocationUseCaseHash,
+      : _$watchLocationUseCaseHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef GetLocationUseCaseRef = AutoDisposeFutureProviderRef<Location>;
+typedef WatchLocationUseCaseRef = AutoDisposeStreamProviderRef<Location>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
